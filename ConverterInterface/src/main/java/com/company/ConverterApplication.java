8821745b35ac185399d2d93b84e9ab99f5fd4651
Sean Monaghan <smonagha@conncoll.edu>
2021-09-19 12:08:38 -0400

    instantiated and useed both the ConverterIf and ConverterSwitch classes in the main method

--- ConverterInterface/src/main/java/com/company/ConverterApplication.java
+++ ConverterInterface/src/main/java/com/company/ConverterApplication.java
@@ -6,5 +6,28 @@ public class ConverterApplication {
         ConverterIf converterIf = new ConverterIf();
         ConverterSwitch converterSwitch = new ConverterSwitch();
 
+        System.out.println("ConverterIf - Converting Numbered Days to Written Days");
+        System.out.println(converterIf.convertDay(4));
+        System.out.println(converterIf.convertDay(-1));
+        System.out.println(converterIf.convertDay(15));
+
+        System.out.println("=======================================");
+        System.out.println("ConverterIf - Converting Numbered Months to Written Months");
+        System.out.println(converterIf.convertMonth(4));
+        System.out.println(converterIf.convertMonth(-1));
+        System.out.println(converterIf.convertMonth(15));
+
+        System.out.println("=======================================");
+        System.out.println("ConverterSwitch - Converting Numbered Days to Written Days");
+        System.out.println(converterSwitch.convertDay(4));
+        System.out.println(converterSwitch.convertDay(-1));
+        System.out.println(converterSwitch.convertDay(15));
+
+        System.out.println("=======================================");
+        System.out.println("ConverterSwitch - Converting Numbered Months to Written Months");
+        System.out.println(converterSwitch.convertMonth(4));
+        System.out.println(converterSwitch.convertMonth(-1));
+        System.out.println(converterSwitch.convertMonth(15));
+
     }
 }
